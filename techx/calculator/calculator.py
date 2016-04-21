19e181a6d89f2e01232a2d0ef9c6b28e7d7bbc0c
Anand Nevase <anand.nevase@xoriant.com>
2016-04-21 10:57:45 -0700

    Update calculator.py
    Remove Divide

--- techx/calculator/calculator.py
+++ techx/calculator/calculator.py
@@ -1,18 +1,11 @@
 class TechxCalculator:
     @staticmethod
     def add(a,b):
-        print 'add'
         return a+b
     
     @staticmethod
     def subtract(a,b):
-        print 'subtract'
         return a-b
-
-    @staticmethod
-    def divide(a,b):
-        print 'subtract'
-        return a/b
     
 
 if __name__ == '__main__':
