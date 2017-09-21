7b896e972bb837bebeb8f42a94bc2201a213b3db
Anand Nevase <anandnevase01@gmail.com>
2017-09-21 16:05:46 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 www.tiaa.org. All rights reserved.
+#
+# Author: Anand Nevase <anand.nevase@in.tiaa.org>
+#
+# Description: Module Description
+
+"""
+Assuming this is file mymodule.py, then this string, being the
+first statement in the file, will become the "mymodule" module's
+docstring when the file is imported.
+"""
+
+
+class MyClass(object):
+
+    """The class's docstring"""
+
+    def my_method(self):
+        """The method's docstring"""
+
+
+def my_function():
+    """The function's docstring"""
